c31af1644118ddc0014f843081d5aad6adc0b4fa
Jo Eyre <joanna@starboardmediauk.co.uk>
2021-05-18 15:59:10 +0100

    Add footer columns
    Added ACF custom fields (in a repeater) that can be used on the options page

--- footer.php
+++ footer.php
@@ -1,12 +1,52 @@
 <footer class="footer">
     <div class="wrapper">
-        <h2>Copyright <?php echo date("Y"); ?></h2>
+        <div class="footer__inner">
+            <div class="footer__col">
+                <h4>Footer title</h4>
+                <nav>
+                    <ul>
+                        <li><a href="#">A link</a> </li>
+                        <li><a href="#">A link</a> </li>
+                        <li><a href="#">A link</a> </li>
+                        <li><a href="#">A link</a> </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="footer__col">
+                <h4>Contact</h4>
+                <address>
+                    <?php the_field('office_address', 'options'); ?>
+                </address>
+            </div>
+            <div class="footer__col">
+                <h4>Social Media</h4>
+                <?php if( have_rows('social_media', 'options') ): ?>
+                    <ul class="social">
+                        <?php while( have_rows('social_media', 'options') ): the_row();
+                            $link = get_sub_field('link');
+                            $icon = get_sub_field('icon');
+                            $text = get_sub_field('text');
+                            ?>
+                            <li>
+                                <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $icon; ?> <span class="hidden"><?php echo $text; ?></span> </a>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom">
+        <div class="wrapper">
+            <div class="footer__bottom__inner">
+                <p>Copyright <?php echo date("Y"); ?></p>
+            </div>
+        </div>
     </div>
 </footer>
 
 <?php wp_footer(); ?>
-<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/assets/js/main-min.js"></script>
+
 
 </body>
 </html>
